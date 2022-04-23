62fa7736baac0a978521611c0da753d9ef904df7
Daniel Soares <daniel.soares@me.com>
2022-04-23 00:08:36 -0300

    fix(server): dev script

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,10 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn']
+  })
+
   app.listen(3010).then(() => console.log('Server is running on port 3010'))
 }
 
